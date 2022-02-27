9a67b558255f70e30ac43d156dc0f829e99e5a80
trpubz <taylor.pubins@me.com>
2022-02-26 22:21:19 -0800

    file load//only works if you debug on initial run and allow access to folders

--- SoupKitchen/Kitchen.swift
+++ SoupKitchen/Kitchen.swift
@@ -14,20 +14,22 @@ struct SoupStew {
     }
     
     func makeStew() {
-        let url = URL(fileURLWithPath: "/Users/trpubz/Documents/_code/SoupKitchen/SoupKitchen/Resources/h2hArticle.html")
-        print(url)
-//        guard let url = (forResource: "h2hArticle", withExtension: ".html") else {
-//            fatalError("Cannot locate file")
-//        }
+        let path = "/Users/tpubz/Documents/_code/SoupKitchen/SoupKitchen/Resources/h2hArticle.html"
+        let url = URL(fileURLWithPath: path)
 
         do {
             let html: String = try String(contentsOf: url, encoding: .utf8)
             print("Successful string conversion")
             let doc: Document = try SwiftSoup.parseBodyFragment(html)
             print("Successful doc conversion")
-            let article: Elements = try doc.getElementsByTag("article")
-            print("Successful article retrival.")
-            print(try article.text())
+            // gets all asides indside doc
+            let aside: Elements = try doc.getElementsByTag("aside")
+            print("Successful aside retrieval.")
+            // The 1st aside is the Top 300 Table. Inside the aside is header and table.
+            let top300Table: Elements = try aside.first()!.select("table")
+            print("Successful 1st table retrieval.")
+            let tbody: Elements = try top300Table.select("tbody")
+            
         } catch Exception.Error(type: let type, Message: let message) {
             print(type)
             print(message)
