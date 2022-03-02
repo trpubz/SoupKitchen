308595bc5474dbdadef4767c6ee552cbf0046fdc
trpubz <taylor.pubins@me.com>
2022-03-01 21:52:12 -0800

    The html file must be stored in /Users/Shared/"file". Select method returns all Elements that match the selection; not children inside the select statement.

--- SoupKitchen/Kitchen.swift
+++ SoupKitchen/Kitchen.swift
@@ -14,11 +14,12 @@ struct SoupStew {
     }
     
     func makeStew() {
-        let path = "/Users/tpubz/Documents/_code/SoupKitchen/SoupKitchen/Resources/h2hArticle.html"
-        let url = URL(fileURLWithPath: path)
+        let file = URL(fileURLWithPath: "/Users/Shared/h2hArticle.html")
+        
+
 
         do {
-            let html: String = try String(contentsOf: url, encoding: .utf8)
+            let html: String = try String(contentsOf: file, encoding: .utf8)
             print("Successful string conversion")
             let doc: Document = try SwiftSoup.parseBodyFragment(html)
             print("Successful doc conversion")
@@ -28,8 +29,9 @@ struct SoupStew {
             // The 1st aside is the Top 300 Table. Inside the aside is header and table.
             let top300Table: Elements = try aside.first()!.select("table")
             print("Successful 1st table retrieval.")
-            let tbody: Elements = try top300Table.select("tbody")
-            
+            // There is only 1 tbody so only 1 element; child members are table rows (tr)
+            let tRows: Elements = try top300Table.select("tbody > tr")
+            print(tRows.count)
         } catch Exception.Error(type: let type, Message: let message) {
             print(type)
             print(message)
